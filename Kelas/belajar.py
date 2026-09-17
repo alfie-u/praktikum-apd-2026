2c51582872857a59d954aca87ef12c692be8c3f7
Alfi <alfifadhilah82@gmail.com>
2026-09-17 16:08:04 +0800

    Coba

--- Kelas/belajar.py
+++ Kelas/belajar.py
@@ -1,1 +1,17 @@
 print("hello world")
+
+print("aku "+"rajin")
+
+buku = {
+'judul' : 'Alfie Hebat',
+'penulis' : 'Alfie',
+'halaman' : 320
+}
+...
+# Mengakses Value dalam Dictionary
+print(buku['judul'])
+print(buku['penulis'])
+print(buku['halaman'])
+
+nama = input("Masukan nama anda: ")
+print(nama)
